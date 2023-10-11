6e8fe0343d942fb7a4f980a2add51a63cdf45dcf
AdamSandyAzhari <147610568+Adamsandyazhai@users.noreply.github.com>
2023-10-11 21:01:24 +0700

    Add files via upload

---
+++ TugasPertemuan1/src/MahasiswaBeraksi.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author adams
+ */
+public class MahasiswaBeraksi {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa();
+        
+        mhs.nama = "Adam Sandy Azhari";
+        mhs.npm = 221108;
+        mhs.kelas = "3C - Informatika";
+        
+        mhs.membaca();
+        mhs.nyontek();
+        mhs.modifikasi();
+    }
+    
+}
+    
+    
+
---
+++ soal2/src/DemoNilai.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author adams
+ */
+public class DemoNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Nilai mhs = new Nilai();
+        
+        mhs.Nama = "Adam Sandy Azhari";
+        mhs.NIM = 221108;
+        mhs.NilaiAbsen = 90;
+        mhs.NilaiTugas = 80;
+        mhs.NilaiUTS = 86;
+        mhs.NilaiUAS = 75;
+        
+        mhs.CetakNilai();
+    }
+    
+}  
+    
